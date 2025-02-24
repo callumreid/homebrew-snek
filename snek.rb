b3ee3bf53f5eb79f724c1956c8123743fbf3d19b
callumreid <callum.reid@volleythat.com>
2025-02-24 11:19:42 -0500

    version right

--- snek.rb
+++ snek.rb
@@ -1,7 +1,7 @@
 class Snek < Formula
   desc "A simple Snake game for the terminal, written in Go"
   homepage "https://github.com/callumreid/snek"
-  url "https://github.com/callumreid/snek/releases/download/v1.0.0/snek-mac"
+  url "https://github.com/callumreid/snek/releases/download/v1.0.1/snek-mac"
   sha256 "1dba06832769dde8797164bc4d06aa3e4931ed1617bdc8a13095e4fba5c55363"
   version "1.0.1"
 
